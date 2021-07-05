fac314a67b76f68055ac96bbf0496e187497d203
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-07-05 21:53:24 +0900

    dice exersise

---
+++ clicker/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{ Component } from 'react';
+import Clicker from './Clicker';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Clicker />
+      </div>
+    )
+  }
+}
+
+export default App;
