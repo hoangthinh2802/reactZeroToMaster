a1f78ecbb9969a9e83201cd50e81242ea1e67ac4
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-07-07 01:21:30 +0900

    state exercise

---
+++ color_boxes/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Box from './Box';
+import BoxContainer from './BoxContainer'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BoxContainer/>
+    </div>
+  );
+}
+
+export default App;
