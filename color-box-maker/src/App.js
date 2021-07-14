40d25dcb45ef132dd66ba4401670276b2dce52eb
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-07-15 00:17:00 +0900

    todo-app

---
+++ color-box-maker/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Box from './Box';
+import BoxList from "./BoxList"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-app/src/Todo.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isEditting: false,
+            task: this.props.task
+        }
+        this.handleRemove = this.handleRemove.bind(this)
+        this.toggleForm = this.toggleForm.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleUpdate = this.handleUpdate.bind(this)
+    }
+    handleRemove(){
+        this.props.removeTodo(this.props.id)
+    }
+    toggleForm(){
+        this.setState({isEditting: !this.state.isEditting})
+    }
+    handleUpdate(evt) {
+        evt.preventDefault();
+        this.props.updateTodo(this.props.id, this.props.task)
+        this.setState({isEditting: false})
+
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+    render() { 
+        let result;
+        if (this.state.isEditting) {
+            result = (
+                <div>
+                    <form onSubmit={this.handleUpdate}>
+                        <input type='text' value={this.state.task} name="task" onChange={this.handleChange} />
+                        <button>Save</button>
+                    </form>
+                </div>
+            )
+        } else {
+            result = (
+                <div>
+                    <button onClick={this.toggleForm}>Edit</button>
+                    <button onClick={this.handleRemove}>X</button>
+                    <li>{this.props.task}</li>
+                </div>
+            )
+        }
+        return result;
+        
+    }
+}
+export default Todo;
