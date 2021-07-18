5c50fb6bbc4cb3d69474f346137dfd6acb74a0bd
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-07-18 22:10:10 +0900

    router-test

---
+++ router-test/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Link, Route, Switch, NavLink } from 'react-router-dom';
+import About from "./About"
+import Dog from './Dog';
+import Contact from './Contact';
+
+import './App.css';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <nav className="App-nav">
+          <NavLink exact activeClassName="active-link" to="/">About</NavLink> 
+          <NavLink exact activeClassName="active-link" to="/dog">Dog</NavLink>
+          <NavLink exact activeClassName="active-link" to="/contact">Contact</NavLink>
+
+        </nav>
+        <Switch>
+          <Route exact path="/" component={About}/>
+          <Route exact path="/dog" component={() => <Dog name="MONEY" />}/>
+          <Route exact path="/contact" component={Contact}/>
+        </Switch>
+      </div>
+    );
+  }
+  }
+  
+
+export default App;
