71ac02ec6624d033c82138549264fbb4276ae81c
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-07-22 01:54:13 +0900

    Color-app part 1

---
+++ colors-app/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import {generatePalette} from "./colorHelpers"
+
+class App extends Component {
+  render(){
+    return (
+    
+      <div className="App">
+        <Palette palette={generatePalette(seedColors[4])}/>
+      </div>
+    );
+  }
+  }
+  
+
+export default App;
---
+++ colors-app/src/Palette.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react"
+import ColorBox from "./ColorBox"
+import "./Palette.css"
+import Slider from "rc-slider"
+import "rc-slider/assets/index.css"
+
+class Palette extends Component{
+    constructor(props) {
+        super(props) 
+        this.state = {level: 500}
+        this.changeLevel = this.changeLevel.bind(this)
+
+        
+    }
+    changeLevel(level){
+        this.setState({ level})
+        console.log(level);
+    }
+    render(){
+        const colorBoxes = this.props.palette.colors[this.state.level].map(color => (
+            <ColorBox background={color.hex} name={color.name} />
+        ))
+        return(
+            <div className="Palette">
+                <div className="slider">
+                    <Slider defaultValue={this.state.level} min={100} max={900}  step={100} onAfterChange={this.changeLevel}/>
+                </div>
+                
+                <div className="Palette-colors">
+                    {colorBoxes}
+                </div>
+                
+            </div>
+        )
+    }
+}
+export default Palette
