8b1c631ccf575b181eacabbf556adf497b2929ca
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-07-22 18:04:43 +0900

    add slackbar

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,30 +1,39 @@
 import React, {Component} from "react"
 import ColorBox from "./ColorBox"
+import Navbar from "./Navbar"
+
 import "./Palette.css"
-import Slider from "rc-slider"
-import "rc-slider/assets/index.css"
+
+
 
 class Palette extends Component{
     constructor(props) {
         super(props) 
-        this.state = {level: 500}
+        this.state = {level: 500, format: "hex"}
         this.changeLevel = this.changeLevel.bind(this)
-
+        this.changeFormat = this.changeFormat.bind(this)
         
     }
     changeLevel(level){
         this.setState({ level})
         console.log(level);
     }
+    changeFormat(val) {
+        this.setState({format: val})
+    }
     render(){
-        const colorBoxes = this.props.palette.colors[this.state.level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
+        const{color} = this.props.palette
+        const {level, format} = this.state
+        const colorBoxes = this.props.palette.colors[level].map(color => (
+            <ColorBox background={color.[format]} name={color.name} />
         ))
         return(
             <div className="Palette">
-                <div className="slider">
-                    <Slider defaultValue={this.state.level} min={100} max={900}  step={100} onAfterChange={this.changeLevel}/>
-                </div>
+                <Navbar 
+                    level={level} 
+                    changeLevel={this.changeLevel} 
+                    handleChange={this.changeFormat}   
+                />
                 
                 <div className="Palette-colors">
                     {colorBoxes}
